c654542a0941daf9112889da9865d63333dbde24
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-26 18:21:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const tutorials = [
 
 
 const titleCased = () => {
-  return tutorials.map( sentence => {
-    let words = sentence.split(' ')
-    let titleized = words.map( word => word.charAt(0).toUpperCase() + word.slice(1) ).join(' ')
     return titleized
-  })
+
 }
